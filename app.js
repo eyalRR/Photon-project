e1c2f24f14920d0747af64e26279348145086153
Eyal <rabeyal123@gmail.com>
2022-06-05 21:00:44 +0300

    finishing

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
 let searchValue;
-
+const more = document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 //Event listener
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
+    currentSearch = searchValue;
     searchPhotos(searchValue);
 })
+more.addEventListener("click", loadMore);
 
 function updateInput(e){
     searchValue = e.target.value;
@@ -46,14 +51,16 @@ function generatePictures(data){
 }
 
 async function curatedPhotos(){
-    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15");
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+    const data = await fetchApi(fetchLink);
     console.log(data);
     generatePictures(data);
 }
 
 async function searchPhotos(query){
     clear();
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15`);
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink);
     console.log(data);
     generatePictures(data);
 }
@@ -61,6 +68,18 @@ async function searchPhotos(query){
 function clear(){
     gallery.innerHTML = '';
     searchInput.value = '';
+}
+
+async function loadMore(){
+    page++;
+    if(currentSearch){
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    }else{
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+    const data = await fetchApi(fetchLink);
+    console.log(data);
+    generatePictures(data);
 }
 
 curatedPhotos();
